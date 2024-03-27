50a326dd9a3e5fa7c3525d2e142dd71881056776
Ciencia de Datos Español <86606384+cienciadedatosesp@users.noreply.github.com>
2024-03-27 19:02:02 -0400

    Add files via upload

---
+++ ggplot class.R
@@ -0,0 +1,517 @@
+#install.packages("Rtools") 
+#install.packages("ggplot2")
+
+# load package and data
+options(scipen=999)  # turn-off scientific notation like 1e+48
+library(ggplot2)
+theme_set(theme_bw())  # pre-set the bw theme.
+data("midwest", package = "ggplot2")
+
+# Scatterplot
+gg <- ggplot(midwest, aes(x=area, y=poptotal)) + 
+  geom_point(aes(col=state, size=popdensity)) + 
+  geom_smooth(method="loess", se=F) + 
+  xlim(c(0, 0.1)) + 
+  ylim(c(0, 500000)) + 
+  labs(subtitle="Area Vs Poblacion", 
+       y="Population", 
+       x="Area", 
+       title="Grafico de Dispersion", 
+       caption = "Fuente: midwest")
+
+plot(gg)
+
+options(scipen = 999)
+library(ggplot2)
+library(ggalt)
+midwest_select <- midwest[midwest$poptotal > 350000 & 
+                            midwest$poptotal <= 500000 & 
+                            midwest$area > 0.01 & 
+                            midwest$area < 0.1, ]
+
+ #install 'ggalt'
+ #devtools::install_github("hrbrmstr/ggalt")
+options(scipen = 999)
+library(ggplot2)
+library(ggalt)
+midwest_select <- midwest[midwest$poptotal > 350000 & 
+                            midwest$poptotal <= 500000 & 
+                            midwest$area > 0.01 & 
+                            midwest$area < 0.1, ]
+
+# Plot
+ggplot(midwest, aes(x=area, y=poptotal)) + 
+  geom_point(aes(col=state, size=popdensity)) +   # draw points
+  geom_smooth(method="loess", se=F) + 
+  xlim(c(0, 0.1)) + 
+  ylim(c(0, 500000)) +   # draw smoothing line
+  geom_encircle(aes(x=area, y=poptotal), 
+                data=midwest_select, 
+                color="red", 
+                size=2, 
+                expand=0.08) +   # encircle
+  labs(subtitle="Area Vs Poblacion", 
+       y="Population", 
+       x="Area", 
+       title="Grafico de Dispersion + Circulo", 
+       caption="Fuente: midwest")
+
+# load package and data
+library(ggplot2)
+data(mpg, package="ggplot2") # alternate source: "http://goo.gl/uEeRGu")
+theme_set(theme_bw())  # pre-set the bw theme.
+
+g <- ggplot(mpg, aes(cty, hwy))
+
+# Scatterplot
+g + geom_point() + 
+  geom_smooth(method="lm", se=F) +
+  labs(subtitle="mpg: ciudad vs Millaje", 
+       y="hwy", 
+       x="cty", 
+       title="Grafico de Dispersion", 
+       caption="Fuente: mpg")
+
+# load package and data
+library(ggplot2)
+data(mpg, package="ggplot2")
+# mpg <- read.csv("http://goo.gl/uEeRGu")
+
+# Scatterplot
+theme_set(theme_bw())  # pre-set the bw theme.
+g <- ggplot(mpg, aes(cty, hwy))
+g + geom_count(col="tomato3", show.legend=F) +
+  labs(subtitle="mpg: ciudad vs millaje", 
+       y="hwy", 
+       x="cty", 
+       title="Grafico de cuenta")
+
+mpg_select <- mpg[mpg$manufacturer %in% c("audi", "ford", "honda", "hyundai"), ]
+
+# Scatterplot
+theme_set(theme_bw())  # pre-set the bw theme.
+g <- ggplot(mpg_select, aes(displ, cty)) + 
+  labs(subtitle="mpg: Desplazamiento vs Millaje",
+       title="Grafico de burbuja")
+
+g + geom_jitter(aes(col=manufacturer, size=hwy)) + 
+  geom_smooth(aes(col=manufacturer), method="lm", se=F)
+
+
+# Source: https://github.com/dgrtwo/gganimate
+ install.packages("cowplot")  # a gganimate dependency
+# devtools::install_github("dgrtwo/gganimate")
+library(ggplot2)
+ #install.packages('devtools')
+ #library(installR)
+ #install.Rtools()
+library(gganimate)
+library(gapminder)
+devtools::install_github("thomasp85/gganimate", ref="v0.1.1") 
+ 
+theme_set(theme_bw())  # pre-set the bw theme.
+
+g <- ggplot(gapminder, aes(gdpPercap, lifeExp, size = pop, frame = year)) +
+  geom_point() +
+  geom_smooth(aes(group = year), 
+              method = "lm", 
+              show.legend = FALSE) +
+  facet_wrap(~continent, scales = "free") +
+  scale_x_log10()  # convert to log scale
+
+gganimate(g, interval=0.2)
+
+# load package and data
+library(ggplot2)
+library(ggExtra)
+data(mpg, package="ggplot2")
+# mpg <- read.csv("http://goo.gl/uEeRGu")
+
+# Scatterplot
+theme_set(theme_bw())  # pre-set the bw theme.
+mpg_select <- mpg[mpg$hwy >= 35 & mpg$cty > 27, ]
+g <- ggplot(mpg, aes(cty, hwy)) + 
+  geom_count() + 
+  geom_smooth(method="lm", se=F)
+
+ggMarginal(g, type = "histogram", fill="transparent")
+ggMarginal(g, type = "boxplot", fill="transparent")
+# ggMarginal(g, type = "density", fill="transparent")
+
+# devtools::install_github("kassambara/ggcorrplot")
+library(ggplot2)
+library(ggcorrplot)
+
+# Correlation matrix
+data(mtcars)
+corr <- round(cor(mtcars), 1)
+
+# Plot
+ggcorrplot(corr, hc.order = TRUE, 
+           type = "lower", 
+           lab = TRUE, 
+           lab_size = 3, 
+           method="circle", 
+           colors = c("tomato2", "white", "springgreen3"), 
+           title="Correlograma de mtcars", 
+           ggtheme=theme_bw)
+
+
+library(ggplot2)
+theme_set(theme_bw())  
+
+# Data Prep
+data("mtcars")  # load data
+mtcars$`car name` <- rownames(mtcars)  # create new column for car names
+mtcars$mpg_z <- round((mtcars$mpg - mean(mtcars$mpg))/sd(mtcars$mpg), 2)  # compute normalized mpg
+mtcars$mpg_type <- ifelse(mtcars$mpg_z < 0, "below", "above")  # above / below avg flag
+mtcars <- mtcars[order(mtcars$mpg_z), ]  # sort
+mtcars$`car name` <- factor(mtcars$`car name`, levels = mtcars$`car name`)  # convert to factor to retain sorted order in plot.
+
+# Diverging Barcharts
+ggplot(mtcars, aes(x=`car name`, y=mpg_z, label=mpg_z)) + 
+  geom_bar(stat='identity', aes(fill=mpg_type), width=.5)  +
+  scale_fill_manual(name="Millaje", 
+                    labels = c("Encima Promedio", "Debajo Promedio"), 
+                    values = c("above"="#00ba38", "below"="#f8766d")) + 
+  labs(subtitle="Millaje Normalizado de 'mtcars'", 
+       title= "Barras Diverg") + 
+  coord_flip()
+
+
+#lollipop chart
+library(ggplot2)
+theme_set(theme_bw())
+
+ggplot(mtcars, aes(x=`car name`, y=mpg_z, label=mpg_z)) + 
+  geom_point(stat='identity', fill="black", size=6)  +
+  geom_segment(aes(y = 0, 
+                   x = `car name`, 
+                   yend = mpg_z, 
+                   xend = `car name`), 
+               color = "black") +
+  geom_text(color="white", size=2) +
+  labs(title="Grafico de Paleta Divergente", 
+       subtitle="Millaje Normalizado de 'mtcars': Paleta") + 
+  ylim(-2.5, 2.5) +
+  coord_flip()
+
+#preparing data to do graph
+# Prepare data: group mean city mileage by manufacturer.
+cty_mpg <- aggregate(mpg$cty, by=list(mpg$manufacturer), FUN=mean)  # aggregate
+colnames(cty_mpg) <- c("make", "mileage")  # change column names
+cty_mpg <- cty_mpg[order(cty_mpg$mileage), ]  # sort
+cty_mpg$make <- factor(cty_mpg$make, levels = cty_mpg$make)  # to retain the order in plot.
+head(cty_mpg, 4)
+
+library(ggplot2)
+theme_set(theme_bw())
+
+# Draw plot
+ggplot(cty_mpg, aes(x=make, y=mileage)) + 
+  geom_bar(stat="identity", width=.5, fill="tomato3") + 
+  labs(title="Ordered Bar Chart", 
+       subtitle="Marca Vs Millaje Prom.", 
+       caption="source: mpg") + 
+  theme(axis.text.x = element_text(angle=65, vjust=0.6))
+
+#otra version
+library(ggplot2)
+library(scales)
+theme_set(theme_classic())
+
+# Plot
+ggplot(cty_mpg, aes(x=make, y=mileage)) + 
+  geom_point(col="tomato2", size=3) +   # Draw points
+  geom_segment(aes(x=make, 
+                   xend=make, 
+                   y=min(mileage), 
+                   yend=max(mileage)), 
+               linetype="dashed", 
+               size=0.1) +   # Draw dashed lines
+  labs(title="Grafico de Puntos", 
+       subtitle="Marca Vs Millaje Prom.", 
+       caption="source: mpg") +  
+  coord_flip()
+
+#cambio en el tiempo GDP
+library(scales)
+theme_set(theme_classic())
+
+# prep data
+df <- read.csv("https://raw.githubusercontent.com/selva86/datasets/master/gdppercap.csv")
+colnames(df) <- c("continent", "1952", "1957")
+left_label <- paste(df$continent, round(df$`1952`),sep=", ")
+right_label <- paste(df$continent, round(df$`1957`),sep=", ")
+df$class <- ifelse((df$`1957` - df$`1952`) < 0, "red", "green")
+
+# Plot
+p <- ggplot(df) + geom_segment(aes(x=1, xend=2, y=`1952`, yend=`1957`, col=class), size=.75, show.legend=F) + 
+  geom_vline(xintercept=1, linetype="dashed", size=.1) + 
+  geom_vline(xintercept=2, linetype="dashed", size=.1) +
+  scale_color_manual(labels = c("Up", "Down"), 
+                     values = c("green"="#00ba38", "red"="#f8766d")) +  # color of lines
+  labs(x="", y="Promedio GdpPerCap") +  # Axis labels
+  xlim(.5, 2.5) + ylim(0,(1.1*(max(df$`1952`, df$`1957`))))  # X and Y axis limits
+
+# Add texts
+p <- p + geom_text(label=left_label, y=df$`1952`, x=rep(1, NROW(df)), hjust=1.1, size=3.5)
+p <- p + geom_text(label=right_label, y=df$`1957`, x=rep(2, NROW(df)), hjust=-0.1, size=3.5)
+p <- p + geom_text(label="Time 1", x=1, y=1.1*(max(df$`1952`, df$`1957`)), hjust=1.2, size=5)  # title
+p <- p + geom_text(label="Time 2", x=2, y=1.1*(max(df$`1952`, df$`1957`)), hjust=-0.1, size=5)  # title
+
+# Minify theme
+p + theme(panel.background = element_blank(), 
+          panel.grid = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text.x = element_blank(),
+          panel.border = element_blank(),
+          plot.margin = unit(c(1,2,1,2), "cm"))
+
+#histograma con diferentes clases
+library(ggplot2)
+theme_set(theme_classic())
+
+# Histogram on a Categorical variable
+g <- ggplot(mpg, aes(manufacturer))
+g + geom_bar(aes(fill=class), width = 0.5) + 
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  labs(title="Histogram on Categorical Variable", 
+       subtitle="Manufacturer across Vehicle Classes") 
+
+theme_set(theme_classic())
+
+# Plot
+g <- ggplot(mpg, aes(cty))
+g + geom_density(aes(fill=factor(cyl)), alpha=0.8) + 
+  labs(title="Density plot", 
+       subtitle="City Mileage Grouped by Number of cylinders",
+       caption="Source: mpg",
+       x="City Mileage",
+       fill="# Cylinders")
+
+theme_set(theme_classic())
+
+# Plot
+g <- ggplot(mpg, aes(class, cty))
+g + geom_boxplot(varwidth=T, fill="plum") + 
+  labs(title="Box plot", 
+       subtitle="City Mileage grouped by Class of vehicle",
+       caption="Source: mpg",
+       x="Class of Vehicle",
+       y="City Mileage")
+
+#violin
+theme_set(theme_bw())
+
+# plot
+g <- ggplot(mpg, aes(class, cty))
+g + geom_violin() + 
+  labs(title="Violin plot", 
+       subtitle="City Mileage vs Class of vehicle",
+       caption="Source: mpg",
+       x="Class of Vehicle",
+       y="City Mileage")
+
+library(ggplot2)
+theme_set(theme_classic())
+
+# Source: Frequency table
+df <- as.data.frame(table(mpg$class))
+colnames(df) <- c("class", "freq")
+pie <- ggplot(df, aes(x = "", y=freq, fill = factor(class))) + 
+  geom_bar(width = 1, stat = "identity") +
+  theme(axis.line = element_blank(), 
+        plot.title = element_text(hjust=0.5)) + 
+  labs(fill="class", 
+       x=NULL, 
+       y=NULL, 
+       title="Pie Chart de clase", 
+       caption="Source: mpg")
+
+pie + coord_polar(theta = "y", start=0)
+
+# Source: Categorical variable.
+# mpg$class
+pie <- ggplot(mpg, aes(x = "", fill = factor(class))) + 
+  geom_bar(width = 1) +
+  theme(axis.line = element_blank(), 
+        plot.title = element_text(hjust=0.5)) + 
+  labs(fill="class", 
+       x=NULL, 
+       y=NULL, 
+       title="Pie Chart of class", 
+       caption="Source: mpg")
+
+pie + coord_polar(theta = "y", start=0)
+
+#bar chart
+# prep frequency table
+freqtable <- table(mpg$manufacturer)
+df <- as.data.frame.table(freqtable)
+head(df)
+
+# plot
+library(ggplot2)
+theme_set(theme_classic())
+
+# Plot
+g <- ggplot(df, aes(Var1, Freq))
+g + geom_bar(stat="identity", width = 0.5, fill="tomato2") + 
+  labs(title="Bar Chart", 
+       subtitle="Manufacturer of vehicles", 
+       caption="Source: Frequency of Manufacturers from 'mpg' dataset") +
+  theme(axis.text.x = element_text(angle=65, vjust=0.6))
+#piramide poblacional
+library(ggplot2)
+library(ggthemes)
+options(scipen = 999)  # turns of scientific notations like 1e+40
+
+#bar with distribution
+# From on a categorical column variable
+g <- ggplot(mpg, aes(manufacturer))
+g + geom_bar(aes(fill=class), width = 0.5) + 
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) +
+  labs(title="Categorywise Bar Chart", 
+       subtitle="Manufacturer of vehicles", 
+       caption="Source: Manufacturers from 'mpg' dataset")
+
+# Read data
+email_campaign_funnel <- read.csv("https://raw.githubusercontent.com/selva86/datasets/master/email_campaign_funnel.csv")
+
+# X Axis Breaks and Labels 
+brks <- seq(-15000000, 15000000, 5000000)
+lbls = paste0(as.character(c(seq(15, 0, -5), seq(5, 15, 5))), "m")
+
+# Plot
+ggplot(email_campaign_funnel, aes(x = Stage, y = Users, fill = Gender)) +   # Fill column
+  geom_bar(stat = "identity", width = .6) +   # draw the bars
+  scale_y_continuous(breaks = brks,   # Breaks
+                     labels = lbls) + # Labels
+  coord_flip() +  # Flip axes
+  labs(title="Email Campaign Funnel") +
+  theme_tufte() +  # Tufte theme from ggfortify
+  theme(plot.title = element_text(hjust = .5), 
+        axis.ticks = element_blank()) +   # Centre plot title
+  scale_fill_brewer(palette = "Dark2")  # Color palette
+
+#data lenguajes programacion
+library(ggplot2) 
+library(treemapify)
+proglangs <- read.csv("https://raw.githubusercontent.com/selva86/datasets/master/proglanguages.csv")
+
+# plot
+treeMapCoordinates <- treemapify(proglangs,
+                                 area = "value",
+                                 fill = "parent",
+                                 label = "id",
+                                 group = "parent")
+
+treeMapPlot <- ggplotify(treeMapCoordinates) + 
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  scale_fill_brewer(palette = "Dark2")
+
+print(treeMapPlot)
+
+#mapas
+# Better install the dev versions ----------
+# devtools::install_github("dkahle/ggmap")
+# devtools::install_github("hrbrmstr/ggalt")
+
+# load packages
+library(ggplot2)
+library(ggmap)
+library(ggalt)
+
+#API key
+
+# this sets your google map permanently
+register_google(key = "AIzaSyAGKqqXWVZ8kWCyJPrScpo_Xon9-Ju6x_Y", write = TRUE)
+
+# Get Chennai's Coordinates --------------------------------
+chennai <-  geocode("Chennai")  # get longitude and latitude
+
+# Get the Map ----------------------------------------------
+# Google Satellite Map
+chennai_ggl_sat_map <- qmap("chennai", zoom=12, source = "google", maptype="satellite")  
+
+# Google Road Map
+chennai_ggl_road_map <- qmap("chennai", zoom=12, source = "google", maptype="roadmap")  
+
+# Google Hybrid Map
+chennai_ggl_hybrid_map <- qmap("chennai", zoom=12, source = "google", maptype="hybrid")  
+
+
+# Get Coordinates for Chennai's Places ---------------------
+chennai_places <- c("Kolathur",
+                    "Washermanpet",
+                    "Royapettah",
+                    "Adyar",
+                    "Guindy")
+
+places_loc <- geocode(chennai_places)  # get longitudes and latitudes
+
+
+
+# Plot Google Road Map -------------------------------------
+chennai_ggl_road_map + geom_point(aes(x=lon, y=lat),
+                                  data = places_loc, 
+                                  alpha = 0.7, 
+                                  size = 7, 
+                                  color = "tomato") + 
+  geom_encircle(aes(x=lon, y=lat),
+                data = places_loc, size = 2, color = "blue")
+
+# Google Hybrid Map ----------------------------------------
+chennai_ggl_hybrid_map + geom_point(aes(x=lon, y=lat),
+                                    data = places_loc, 
+                                    alpha = 0.7, 
+                                    size = 7, 
+                                    color = "tomato") + 
+  geom_encircle(aes(x=lon, y=lat),
+                data = places_loc, size = 2, color = "blue")
+
+#Paris
+# Get  Coordinates --------------------------------
+paris <-  geocode("Paris")  # get longitude and latitude
+
+# Get the Map ----------------------------------------------
+# Google Satellite Map
+paris_ggl_sat_map <- qmap("paris", zoom=12, source = "google", maptype="satellite")  
+
+# Google Road Map
+paris_ggl_road_map <- qmap("paris", zoom=12, source = "google", maptype="roadmap")  
+
+# Google Hybrid Map
+paris_ggl_hybrid_map <- qmap("paris", zoom=12, source = "google", maptype="hybrid")  
+
+
+# Get Coordinates for Places ---------------------
+paris_places <- c("Eiffel Tower",
+                    "Louvre",
+                    "Arc de Triomphe",
+                    "Place de la Concorde",
+                    "Pantheon")
+
+places_loc <- geocode(paris_places)  # get longitudes and latitudes
+
+
+
+# Plot Google Road Map -------------------------------------
+paris_ggl_road_map + geom_point(aes(x=lon, y=lat),
+                                  data = places_loc, 
+                                  alpha = 0.7, 
+                                  size = 7, 
+                                  color = "tomato") + 
+  geom_encircle(aes(x=lon, y=lat),
+                data = places_loc, size = 2, color = "blue")
+
+# Google Hybrid Map ----------------------------------------
+paris_ggl_hybrid_map + geom_point(aes(x=lon, y=lat),
+                                    data = places_loc, 
+                                    alpha = 0.7, 
+                                    size = 7, 
+                                    color = "tomato") + 
+  geom_encircle(aes(x=lon, y=lat),
+                data = places_loc, size = 2, color = "blue")
